a46081ed5bf51317c339aa13d5f3fe098ad2454e
Fabian Telschow <fabian.telschow@posteo.de>
2018-10-29 16:17:23 +0100

    changed the inout such that it is the same as in CopeStes.m

--- CopeSets_SNR.m
+++ CopeSets_SNR.m
@@ -1,35 +1,39 @@
-function [thresh, quantiles, SNR, asymptSD] = CopeSets_SNR( F, c, lvls, Mboot, bdry_type,...
-                                                             method, delta )
-
+function [thresh, quantiles, SNR, asymptSD] = CopeSets_SNR( F, c, lvls, quantEstim,...
+                                                        bdry_type, delta )
 % Computes all ingredients for CoPe sets of the signal-plus-noise-ratio.
 % Input:
-% F:    random field over a domain in R^D, it is an (D+1)-dimensional array,
-%       where the last dimension enumerates the samples
-% c:    threshold for excursions
-% lvls:       vector for which the quantile needs to be estimated
-% Mboot:     amount of bootstrap replicates (default=5e3)
-% bdry_type: currently 'linear', 'erodilation' or 'true' are supported
-% delta:     required, if bdry_type is equal to 'true'. This is the true
-% method:    option for the bootstrap estimator (default='t')
-%            population SNR function given on a D-dimensional array
-%Output:
-% thresh is the threshold lower and upper for the SNR in order to
-% be in the estimated lower and upper excursion sets 
-% quantile is the bootstrapped quantile of the maximum distribution of the 
-% input processes
-% hatdelta is the sample mean of the fields
-% hatsigma is the sample variance of the fields
+%  F:    random field over a domain in R^D, it is an (D+1)-dimensional array,
+%        where the last dimension enumerates the samples
+%  c:    threshold for excursions
+%  lvls:       vector for which the quantile needs to be estimated%  quantEstim: structure containing the name and the parameters for the
+%              quantile estimation method. Choices:
+%               {
+%                quantEstim.name = 'multiplierbootstrap'
+%                quantEstim.params:
+%                   Mboot:     amount of bootstrap replicates (default=5e3)
+%                   method:    option for the bootstrap estimator (default='t')
+%               }
+%   bdry_type: currently 'linear' or 'true' are supported
+
+% delta:     required, if bdry_type is equal to 'true'. This is the
+% Output:
+%  thresh is the threshold lower and upper for the SNR in order to
+%  be in the estimated lower and upper excursion sets 
+%  quantile is the bootstrapped quantile of the maximum distribution of the 
+%  input processes
+%  hatdelta is the sample mean of the fields
+%  hatsigma is the sample variance of the fields
 %
 %__________________________________________________________________________
 % References:
 %__________________________________________________________________________
 % Author: Fabian Telschow (ftelschow@ucsd.edu)
-% Last changes: 10/05/2018
+% Last changes: 29/10/2018
 %__________________________________________________________________________
 
 % Fill in unset optional values.
 switch nargin
-    case 6
+    case 5
         delta     = 666;
 end
 
@@ -50,7 +54,12 @@ switch(bdry_type)
 end
 
 %%%% Estimate the quantiles of the Gaussian process on the boundary
-quantiles = MultiplierBoots( F_bdry, lvls, Mboot, mask, method );
+if strcmp( quantEstim.name, 'MultiplierBootstrap' )
+    quantiles = MultiplierBoots( F_bdry, lvls, ...
+                    quantEstim.params.Mboot, mask, quantEstim.params.weights, quantEstim.params.method );
+else
+    error("Please specify a valid method for quantile estimation")
+end
 
 %%%% Compute upper and lower threshold for CoPE sets
 sF    = size(F);
@@ -63,3 +72,4 @@ thresh(index{:},1) = c - repmat(shiftdim(quantiles, -length(sF)+1), [sF 1])...
                      .*repmat(asymptSD,[1 1 length(lvls)]) / sqrt(N);
 thresh(index{:},2) = c + repmat(shiftdim(quantiles, -length(sF)+1), [sF 1])...
                      .*repmat(asymptSD,[1 1 length(lvls)]) / sqrt(N);
+end
