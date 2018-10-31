b10b33c34577a2dd981480ea3012f86b4bdbac72
Fabian Telschow <fabian.telschow@posteo.de>
2018-10-31 09:14:38 +0100

    changed another annoying bug....

--- ConcatResults.m
+++ ConcatResults.m
@@ -37,35 +37,30 @@ for k = 1:prod(dimR(1:end-1))
             result{k}.covRate.truebdry = nsim*tmp{k}.covRate.truebdry;
             result{k}.covRate.linbdry  = nsim*tmp{k}.covRate.linbdry;
             result{k}.covRate.erodbdry = nsim*tmp{k}.covRate.erodbdry;
-        elseif l==dimR(end)
-            % update the number of simulations
-            nsim = nsim + tmp{k,l}.nsim;
-            % concatinate the estimates of the quantiles
-            result{k}.quant.truebdry     = [result{k}.quant.truebdry tmp{k,l}.quant.truebdry];
-            result{k}.quant.linbdry      = [result{k}.quant.linbdry tmp{k,l}.quant.linbdry];
-            result{k}.quant.erodbdrybdry = [result{k}.quant.erodbdry tmp{k,l}.quant.erodbdry];
-            % save the final number of simualtions to the output
-            result{k}.nsim = nsim;
-            % transform back to covering percentages
-            result{k}.covRate.truebdry = result{k}.covRate.truebdry / nsim;
-            result{k}.covRate.linbdry  = result{k}.covRate.linbdry  / nsim;
-            result{k}.covRate.erodbdry = result{k}.covRate.erodbdry / nsim;
-            % compute the new standard errors of the simulation
-            result{k}.stdErr.rough = sqrt(result{k}.lvls .* (1-result{k}.lvls)/nsim);
-            result{k}.stdErr.truebdry = sqrt(result{k}.covRate.truebdry .* (1-result{k}.covRate.truebdry) / nsim);
-            result{k}.stdErr.linbdry  = sqrt(result{k}.covRate.linbdry  .* (1-result{k}.covRate.linbdry)  / nsim);
-            result{k}.stdErr.erodbdry = sqrt(result{k}.covRate.erodbdry .* (1-result{k}.covRate.erodbdry) / nsim);
         else
             % update the number of simulations
             nsim = nsim + tmp{k,l}.nsim;
             % add the weighted by number of simulations to the already weighted covering rate
-            result{k}.covRate.truebdry = result{k}.covRate.truebdry + tmp{k,l}.nsim * tmp{k}.covRate.truebdry;
-            result{k}.covRate.linbdry  = result{k}.covRate.linbdry  + tmp{k,l}.nsim * tmp{k}.covRate.linbdry;
-            result{k}.covRate.erodbdry = result{k}.covRate.erodbdry + tmp{k,l}.nsim * tmp{k}.covRate.erodbdry;
+            result{k}.covRate.truebdry = result{k}.covRate.truebdry + tmp{k,l}.nsim * tmp{k,l}.covRate.truebdry;
+            result{k}.covRate.linbdry  = result{k}.covRate.linbdry  + tmp{k,l}.nsim * tmp{k,l}.covRate.linbdry;
+            result{k}.covRate.erodbdry = result{k}.covRate.erodbdry + tmp{k,l}.nsim * tmp{k,l}.covRate.erodbdry;
             % concatinate the estimates of the quantiles
             result{k}.quant.truebdry     = [result{k}.quant.truebdry tmp{k,l}.quant.truebdry];
             result{k}.quant.linbdry      = [result{k}.quant.linbdry tmp{k,l}.quant.linbdry];
             result{k}.quant.erodbdrybdry = [result{k}.quant.erodbdry tmp{k,l}.quant.erodbdry];
+            if l==dimR(end)
+                % save the final number of simualtions to the output
+                result{k}.nsim = nsim;
+                % transform back to covering percentages
+                result{k}.covRate.truebdry = result{k}.covRate.truebdry / nsim;
+                result{k}.covRate.linbdry  = result{k}.covRate.linbdry  / nsim;
+                result{k}.covRate.erodbdry = result{k}.covRate.erodbdry / nsim;
+                % compute the new standard errors of the simulation
+                result{k}.stdErr.rough = sqrt(result{k}.lvls .* (1-result{k}.lvls)/nsim);
+                result{k}.stdErr.truebdry = sqrt(result{k}.covRate.truebdry .* (1-result{k}.covRate.truebdry) / nsim);
+                result{k}.stdErr.linbdry  = sqrt(result{k}.covRate.linbdry  .* (1-result{k}.covRate.linbdry)  / nsim);
+                result{k}.stdErr.erodbdry = sqrt(result{k}.covRate.erodbdry .* (1-result{k}.covRate.erodbdry) / nsim);
+            end
         end
     end % loop over the results of a single experiment
 end % loop over the different simulations
