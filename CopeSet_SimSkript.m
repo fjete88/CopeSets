971ef4b10566d6528a8e79bf2f791e060daf0e35
Fabian Telschow <fabian.telschow@posteo.de>
2018-11-02 08:58:52 +0100

    added the 100x50 simulations

--- CopeSet_SimSkript.m
+++ CopeSet_SimSkript.m
@@ -203,7 +203,7 @@ save('simulations/ResultSim5050regNsim3000maxN240_isotropic_FWHM.mat', ...
 clear all
 close all
 % Sample size and simulation size
-nvec = [30 60 120 240];
+nvec = [30 60 120 240 300];
 % Considered confidence levels
 lvls = [0.85 0.90 0.95];
 % threshold for the simulation
@@ -216,16 +216,14 @@ quantEstim = struct('name', "MultiplierBootstrap",...
                                              'weights', "gaussian",...   
                                              'method', 't')...
                                                 );
-nsim = 1e3;
+nsim = 3e3;
 dim  = [100 50];
 FWHM = [5 10];
 a    = [[1 3]; [0 5]];
-
-% define index to access spatial domain of the fields
-index  = repmat( {':'}, 1, length(dim) );
+batchnumber = 50;
 
 % define cell array to store the results of the simulations
-sim10050tResults = cell(length(FWHM), size(a,2), length(nvec), 3);
+sim10050tResults = cell(length(FWHM), size(a,2), length(nvec));
 
 %%%%%% simulations of covering rates
 % loop over the smoothing parameter
@@ -233,44 +231,38 @@ sim10050tResults = cell(length(FWHM), size(a,2), length(nvec), 3);
 countf = 0;
 for f = FWHM
     countf = countf+1;
-    % loop over precomputed errorfields
-    for i = 1:3
-        % load precomputed data
-        load(['errorfields/dim',num2str(dim(1)), num2str(dim(2)),'Nsim1000LN240_isotropic_FWHM',...
-                num2str(f),'_', num2str(i), '.mat'])
-        % loop over hte slope of the ramp signal
-        counta = 0;
-        for a = [1 4]
-            counta = counta+1;
-            % specify the paramter for the slope of the ramp
-            if a==1
-                aa = [1 3];
-            else
-                aa = a;
-            end
-            % parameters for the linear ramp
-            paramSignal = struct('shape', "linear", 'shapeparam', aa, 'type', "signal");
-            % loop over the sample size
-            countn = 0;
-            for n = nvec
-                countn = countn+1;
-                sim10050tResults{countf, counta, countn, i} = CopeSets_sim( nsim, n, lvls, paramSignal, c,...
-                                            paramNoise, quantEstim, eps(index{:},1:n, :), pool_num );    
-            end % end loop over sample size
-        end % end loop over slope of the ramp
-        size(sim10050tResults)
-        save('simulations/sim10050tNsim3000maxN240_isotropic_FWHM.mat', ...
-                'sim10050tResults', '-v7.3')
-    end % end over loop of precomputed errorfields
+    paramNoise = struct( 'FWHM', [f, f], 'dim', dim, 'noise', "normal", 'nu', '',...
+                         'kernel', "gauss", 'bin', 0, 'sd', ones(dim) );
+    % loop over hte slope of the ramp signal
+    counta = 0;
+    for a = [1 4]
+        counta = counta+1;
+        % specify the paramter for the slope of the ramp
+        if a==1
+            aa = [1 3];
+        else
+            aa = a;
+        end
+        % parameters for the linear ramp
+        paramSignal = struct('shape', "linear", 'shapeparam', aa, 'type', "signal");
+        % loop over the sample size
+        countn = 0;
+        tic
+        for n = nvec
+            countn = countn+1;
+            sim10050tResults{countf, counta, countn} = CopeSets_sim( nsim, n, lvls, paramSignal, c,...
+                                        paramNoise, quantEstim, batchnumber, pool_num );    
+        end % end loop over sample size
+        toc
+        save('simulations/ResultSim10050tNsim3000maxN240_isotropic_FWHM.mat', ...
+             'sim10050tResults', '-v7.3')
+    end % end loop over slope of the ramp
 end % end loop over the smoothing parameter
 
-save('simulations/sim10050tNsim3000maxN240_isotropic_FWHM.mat', ...
-                'sim10050tResults', '-v7.3')
-            
-% compute the concatinated results and save
-sim10050tResult = ConcatResults(sim10050tResults);
+%%%% save final results
 save('simulations/ResultSim10050tNsim3000maxN240_isotropic_FWHM.mat', ...
-                'sim10050tResult', '-v7.3');
+                'sim10050tResults', '-v7.3')
+    
             
 %%%%%%%%%%% 100x50 domain simulation quantile estim regular multiplier
 %%%%%%%%%%% bootstrap
@@ -278,7 +270,7 @@ save('simulations/ResultSim10050tNsim3000maxN240_isotropic_FWHM.mat', ...
 clear all
 close all
 % Sample size and simulation size
-nvec = [30 60 120 240];
+nvec = [30 60 120 240 300];
 % Considered confidence levels
 lvls = [0.85 0.90 0.95];
 % threshold for the simulation
@@ -291,16 +283,14 @@ quantEstim = struct('name', "MultiplierBootstrap",...
                                              'weights', "gaussian",...   
                                              'method', 'regular')...
                                                 );
-nsim = 1e3;
+nsim = 3e3;
 dim  = [100 50];
 FWHM = [5 10];
 a    = [[1 3]; [0 5]];
-
-% define index to access spatial domain of the fields
-index  = repmat( {':'}, 1, length(dim) );
+batchnumber = 50;
 
 % define cell array to store the results of the simulations
-sim10050regResults = cell(length(FWHM), size(a,2), length(nvec), 3);
+sim10050regResults = cell(length(FWHM), size(a,2), length(nvec));
 
 %%%%%% simulations of covering rates
 % loop over the smoothing parameter
@@ -308,39 +298,168 @@ sim10050regResults = cell(length(FWHM), size(a,2), length(nvec), 3);
 countf = 0;
 for f = FWHM
     countf = countf+1;
-    % loop over precomputed errorfields
-    for i = 1:3
-        % load precomputed data
-        load(['errorfields/dim',num2str(dim(1)), num2str(dim(2)),'Nsim1000LN240_isotropic_FWHM',...
-                num2str(f),'_', num2str(i), '.mat'])
-        % loop over hte slope of the ramp signal
-        counta = 0;
-        for a = [1 4]
-            counta = counta+1;
-            % specify the paramter for the slope of the ramp
-            if a==1
-                aa = [1 3];
-            else
-                aa = a;
-            end
-            % parameters for the linear ramp
-            paramSignal = struct('shape', "linear", 'shapeparam', aa, 'type', "signal");
-            % loop over the sample size
-            countn = 0;
-            for n = nvec
-                countn = countn+1;
-                sim10050regResults{countf, counta, countn, i} = CopeSets_sim( nsim, n, lvls, paramSignal, c,...
-                                            paramNoise, quantEstim, eps(index{:},1:n, :), pool_num );    
-            end % end loop over sample size
-        end % end loop over slope of the ramp
-        size(sim10050regResults)
-        save('simulations/sim10050regNsim3000maxN240_isotropic_FWHM.mat', ...
-                'sim10050regResults', '-v7.3')
-    end % end over loop of precomputed errorfields
+    paramNoise = struct( 'FWHM', [f, f], 'dim', dim, 'noise', "normal", 'nu', '',...
+                         'kernel', "gauss", 'bin', 0, 'sd', ones(dim) );
+    % loop over hte slope of the ramp signal
+    counta = 0;
+    for a = [1 4]
+        counta = counta+1;
+        % specify the paramter for the slope of the ramp
+        if a==1
+            aa = [1 3];
+        else
+            aa = a;
+        end
+        % parameters for the linear ramp
+        paramSignal = struct('shape', "linear", 'shapeparam', aa, 'type', "signal");
+        % loop over the sample size
+        countn = 0;
+        tic
+        for n = nvec
+            countn = countn+1;
+            sim10050regResults{countf, counta, countn} = CopeSets_sim( nsim, n, lvls, paramSignal, c,...
+                                        paramNoise, quantEstim, batchnumber, pool_num );    
+        end % end loop over sample size
+        toc
+        save('simulations/ResultSim10050regNsim3000maxN240_isotropic_FWHM.mat', ...
+             'sim10050regResults', '-v7.3')
+    end % end loop over slope of the ramp
 end % end loop over the smoothing parameter
-save('simulations/sim10050regNsim3000maxN240_isotropic_FWHM.mat', ...
-                'sim10050regResults', '-v7.3')
-% concatinate simulation results and save these results
-sim10050regResult = ConcatResults(sim5050regResults);
+
+%%%% save final results
 save('simulations/ResultSim10050regNsim3000maxN240_isotropic_FWHM.mat', ...
-                'sim10050regResult', '-v7.3');       
+                'sim10050regResults', '-v7.3')
+
+%% %%%%%%%%%%% 100x50 domain simulation
+%%%%%% simulation paramters
+clear all
+close all
+% Sample size and simulation size
+nvec = [30 60 120 240]% 300];
+% Considered confidence levels
+lvls = [0.85 0.90 0.95];
+% threshold for the simulation
+c    = 2;
+% number of parallel computing units
+pool_num = 1;
+% quantile estimation parameters
+quantEstim = struct('name', "MultiplierBootstrap",...
+                            'params', struct('Mboot', 5e3,...
+                                             'weights', "gaussian",...   
+                                             'method', 't')...
+                                                );
+nsim = 2e3;%3e3;
+dim  = [200 50];
+FWHM = [5 10];
+a    = [[1 3]; [0 5]];
+batchnumber = 50;
+
+% define cell array to store the results of the simulations
+sim20050tResults = cell(length(FWHM), size(a,2), length(nvec));
+
+%%%%%% simulations of covering rates
+% loop over the smoothing parameter
+% Initialize counter for filling the results cell structure
+countf = 0;
+for f = FWHM
+    countf = countf+1;
+    paramNoise = struct( 'FWHM', [f, f], 'dim', dim, 'noise', "normal", 'nu', '',...
+                         'kernel', "gauss", 'bin', 0, 'sd', ones(dim) );
+    % loop over hte slope of the ramp signal
+    counta = 0;
+    for a = [1 4]
+        counta = counta+1;
+        % specify the paramter for the slope of the ramp
+        if a==1
+            aa = [1 3];
+        else
+            aa = a;
+        end
+        % parameters for the linear ramp
+        paramSignal = struct('shape', "linear", 'shapeparam', aa, 'type', "signal");
+        % loop over the sample size
+        countn = 0;
+        tic
+        for n = nvec
+            countn = countn+1;
+            tic
+            sim20050tResults{countf, counta, countn} = CopeSets_sim( nsim, n, lvls, paramSignal, c,...
+                                        paramNoise, quantEstim, batchnumber, pool_num );
+            toc
+        end % end loop over sample size
+        toc
+        save('simulations/ResultSim20050tNsim3000maxN240_isotropic_FWHM.mat', ...
+             'sim20050tResults', '-v7.3')
+    end % end loop over slope of the ramp
+end % end loop over the smoothing parameter
+
+%%%% save final results
+save('simulations/ResultSim20050tNsim3000maxN240_isotropic_FWHM.mat', ...
+                'sim20050tResults', '-v7.3')
+    
+            
+%%%%%%%%%%% 100x50 domain simulation quantile estim regular multiplier
+%%%%%%%%%%% bootstrap
+%%%%%% simulation paramters
+clear all
+close all
+% Sample size and simulation size
+nvec = [30 60 120 240 300];
+% Considered confidence levels
+lvls = [0.85 0.90 0.95];
+% threshold for the simulation
+c    = 2;
+% number of parallel computing units
+pool_num = 1;
+% quantile estimation parameters
+quantEstim = struct('name', "MultiplierBootstrap",...
+                            'params', struct('Mboot', 5e3,...
+                                             'weights', "gaussian",...   
+                                             'method', 'regular')...
+                                                );
+nsim = 3e3;
+dim  = [200 50];
+FWHM = [5 10];
+a    = [[1 3]; [0 5]];
+batchnumber = 50;
+
+% define cell array to store the results of the simulations
+sim20050regResults = cell(length(FWHM), size(a,2), length(nvec));
+
+%%%%%% simulations of covering rates
+% loop over the smoothing parameter
+% Initialize counter for filling the results cell structure
+countf = 0;
+for f = FWHM
+    countf = countf+1;
+    paramNoise = struct( 'FWHM', [f, f], 'dim', dim, 'noise', "normal", 'nu', '',...
+                         'kernel', "gauss", 'bin', 0, 'sd', ones(dim) );
+    % loop over hte slope of the ramp signal
+    counta = 0;
+    for a = [1 4]
+        counta = counta+1;
+        % specify the paramter for the slope of the ramp
+        if a==1
+            aa = [1 3];
+        else
+            aa = a;
+        end
+        % parameters for the linear ramp
+        paramSignal = struct('shape', "linear", 'shapeparam', aa, 'type', "signal");
+        % loop over the sample size
+        countn = 0;
+        tic
+        for n = nvec
+            countn = countn+1;
+            sim20050regResults{countf, counta, countn} = CopeSets_sim( nsim, n, lvls, paramSignal, c,...
+                                        paramNoise, quantEstim, batchnumber, pool_num );    
+        end % end loop over sample size
+        toc
+        save('simulations/ResultSim20050regNsim3000maxN240_isotropic_FWHM.mat', ...
+             'sim20050regResults', '-v7.3')
+    end % end loop over slope of the ramp
+end % end loop over the smoothing parameter
+
+%%%% save final results
+save('simulations/ResultSim20050regNsim3000maxN240_isotropic_FWHM.mat', ...
+                'sim20050regResults', '-v7.3')
--- PlotResultsScript.m
+++ PlotResultsScript.m
@@ -1,6 +1,7 @@
 clear all
 close all
 
+%% Visualize 50x50 simulations
 load('simulations/ResultSim5050regNsim3000maxN240_isotropic_FWHM.mat')
 load('simulations/ResultSim5050tNsim3000maxN240_isotropic_FWHM.mat')
 
@@ -9,12 +10,18 @@ PlotResults( sim5050regResult, 'pics/5050regerodResult', 'erodbdry', 'CovRate: R
 PlotResults( sim5050regResult, 'pics/5050regtrueResult', 'truebdry', 'CovRate: Reg. boots/truedbdry Estim')
 
 PlotResults( sim5050tResult, 'pics/5050tlinResult', 'linbdry', 'CovRate: t-boots/linbdry Estim')
-PlotResults( sim5050tResult, 'pics/5050tregResult', 'erodbdry', 'CovRate: t-boots/erodbdry Estim')
-PlotResults( sim5050tResult, 'pics/5050terodResult', 'truebdry', 'CovRate: t-boots/truedbdry Estim')
+PlotResults( sim5050tResult, 'pics/5050terodResult', 'erodbdry', 'CovRate: t-boots/erodbdry Estim')
+PlotResults( sim5050tResult, 'pics/5050ttrueResult', 'truebdry', 'CovRate: t-boots/truedbdry Estim')
 
-i=2;
-j=4;
+%% Visualize 100x50 simulations
+load('simulations/ResultSim10050regNsim3000maxN240_isotropic_FWHM.mat')
+load('simulations/ResultSim10050tNsim3000maxN240_isotropic_FWHM.mat')
 
-A =[[sim5050regResult{1,i,j}.paramNoise.FWHM' sim5050regResult{1,i,j}.covRate.linbdry];...
-    [sim5050regResult{2,i,j}.paramNoise.FWHM' sim5050regResult{2,i,j}.covRate.linbdry]];
-A
+PlotResults( sim10050regResults, 'pics/10050reglinResult', 'linbdry', 'CovRate: Reg. boots/linbdry Estim')
+PlotResults( sim10050regResults, 'pics/10050regerodResult', 'erodbdry', 'CovRate: Reg. boots/erodbdry Estim')
+PlotResults( sim10050regResults, 'pics/10050regtrueResult', 'truebdry', 'CovRate: Reg. boots/truedbdry Estim')
+
+PlotResults( sim10050tResults, 'pics/10050tlinResult', 'linbdry', 'CovRate: t-boots/linbdry Estim')
+PlotResults( sim10050tResults, 'pics/10050terodResult', 'erodbdry', 'CovRate: t-boots/erodbdry Estim')
+PlotResults( sim10050tResults, 'pics/10050ttrueResult', 'truebdry', 'CovRate: t-boots/truedbdry Estim')
+
