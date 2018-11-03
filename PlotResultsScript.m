d55df42ec050935859364308dbc343604dc614a8
Fabian Telschow <fabian.telschow@posteo.de>
2018-11-03 21:59:08 +0100

    all simulations for linear ramp added

--- PlotResultsScript.m
+++ PlotResultsScript.m
@@ -1,39 +1,62 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%%         This script plots the results from CopeSets_SimSkript.m
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all
 close all
 
 cd /home/drtea/Research/MatlabPackages/CopeSets
-%% Visualize 50x50 simulations
-load('simulations/ResultSim5050regNsim3000maxN240_isotropic_FWHM.mat')
-load('simulations/ResultSim5050tNsim3000maxN240_isotropic_FWHM.mat')
 
-PlotResults( sim5050regResult, 'pics/5050reglinResult', 'linbdry', 'CovRate: Reg. boots/linbdry Estim')
-PlotResults( sim5050regResult, 'pics/5050regerodResult', 'erodbdry', 'CovRate: Reg. boots/erodbdry Estim')
-PlotResults( sim5050regResult, 'pics/5050regtrueResult', 'truebdry', 'CovRate: Reg. boots/truedbdry Estim')
+simpathvector = ["simulations/ResultSim50050Nsim3000maxN240isotropic_boot_regulargaussian.mat",...
+                 "simulations/ResultSim50050Nsim3000maxN240isotropic_boot_tgaussian.mat",...
+                 "simulations/ResultSim50050Nsim3000maxN240isotropic_boot_regularrademacher.mat",...
+                 "simulations/ResultSim10050Nsim3000maxN300isotropic_boot_regulargaussian.mat",...
+                 "simulations/ResultSim10050Nsim3000maxN300isotropic_boot_tgaussian.mat",...
+                 "simulations/ResultSim5050Nsim3000maxN300isotropic_boot_regularrademacher.mat",...
+                 "simulations/ResultSim5050Nsim3000maxN300isotropic_boot_trademacher.mat",...
+                 "simulations/ResultSim5050Nsim3000maxN300isotropic_boot_regulargaussian.mat",...
+                 "simulations/ResultSim5050Nsim3000maxN300isotropic_boot_tgaussian.mat"];
 
-PlotResults( sim5050tResult, 'pics/5050tlinResult', 'linbdry', 'CovRate: t-boots/linbdry Estim')
-PlotResults( sim5050tResult, 'pics/5050terodResult', 'erodbdry', 'CovRate: t-boots/erodbdry Estim')
-PlotResults( sim5050tResult, 'pics/5050ttrueResult', 'truebdry', 'CovRate: t-boots/truedbdry Estim')
 
-%% Visualize 100x50 simulations
-load('simulations/ResultSim10050regNsim3000maxN240_isotropic_FWHM.mat')
-load('simulations/ResultSim10050tNsim3000maxN240_isotropic_FWHM.mat')
+%% Visualize 500x50 simulations Rademacher regular
+for simpath = simpathvector
+    % Plot regular results
+    load(simpath)
 
-PlotResults( sim10050regResults, 'pics/10050reglinResult', 'linbdry', 'CovRate: Reg. boots/linbdry Estim')
-PlotResults( sim10050regResults, 'pics/10050regerodResult', 'erodbdry', 'CovRate: Reg. boots/erodbdry Estim')
-PlotResults( sim10050regResults, 'pics/10050regtrueResult', 'truebdry', 'CovRate: Reg. boots/truedbdry Estim')
+    dim     = simResults{1}.paramNoise.dim;
+    method  = simResults{1}.quantEstim.params.method;
+    weights = simResults{1}.quantEstim.params.weights;
 
-PlotResults( sim10050tResults, 'pics/10050tlinResult', 'linbdry', 'CovRate: t-boots/linbdry Estim')
-PlotResults( sim10050tResults, 'pics/10050terodResult', 'erodbdry', 'CovRate: t-boots/erodbdry Estim')
-PlotResults( sim10050tResults, 'pics/10050ttrueResult', 'truebdry', 'CovRate: t-boots/truedbdry Estim')
+    % remove spaces in string
+    tmp = num2str(dim);
+    tmp = tmp(find(~isspace(tmp)));
+    % define path identifier for plots
+    picpathID = strcat( 'pics/', tmp, method, weights );
 
-%% Visualize 100x50 simulations
-%load('simulations/ResultSim10050regNsim3000maxN240_isotropic_FWHM.mat')
-load('simulations/ResultSim10050tNsim3000maxN240_isotropic_FWHM.mat')
+    if strcmp( method, 'regular' )
+        methodflag = ' Reg. ';
+    else
+        methodflag = ' t ';
+    end    
+    if strcmp( weights, 'rademacher' )
+        weightsflag = 'Rad.';
+    else
+        weightsflag = 'Gauss';
+    end
 
-%PlotResults( sim10050regResults, 'pics/10050reglinResult', 'linbdry', 'CovRate: Reg. boots/linbdry Estim')
-%PlotResults( sim10050regResults, 'pics/10050regerodResult', 'erodbdry', 'CovRate: Reg. boots/erodbdry Estim')
-%PlotResults( sim10050regResults, 'pics/10050regtrueResult', 'truebdry', 'CovRate: Reg. boots/truedbdry Estim')
+    titleSnippet = strcat(tmp,methodflag, weightsflag);
+    clear weightsflag methodflag tmp;
 
-PlotResults( sim10050tResults, 'pics/10050tlinResult', 'linbdry', 'CovRate: t-boots/linbdry Estim')
-PlotResults( sim10050tResults, 'pics/10050terodResult', 'erodbdry', 'CovRate: t-boots/erodbdry Estim')
-PlotResults( sim10050tResults, 'pics/10050ttrueResult', 'truebdry', 'CovRate: t-boots/truedbdry Estim')
+    %%% plot results for regular bootstrap
+    % cov rates
+    PlotResults( simResults, strcat(picpathID, 'linResult'), 'linbdry', ...
+                 strcat(titleSnippet,'/linbdry'), 'y', 'variable')
+    PlotResults( simResults, strcat(picpathID, 'trueResult')', 'truebdry', ...
+                strcat(titleSnippet,'/truedbdry'), 'y', 'variable')
+    %PlotResults( sim5050regResult, 'pics/5050regGausserodResult', 'erodbdry', 'CovRate: Reg. boots/erodbdry Estim', 'static')
+    % quantile estimates
+    PlotQuantiles( simResults, strcat(picpathID, 'meanQuantiles'), titleSnippet)
+
+    close all
+end
