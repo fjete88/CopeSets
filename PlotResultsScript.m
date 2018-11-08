7895a09f5b096d9c5a1a45cf9097a7715bbc9f2d
Fabian Telschow <fabian.telschow@posteo.de>
2018-11-08 10:15:16 +0100

    introduced more comments to improve readibility

--- PlotResultsScript.m
+++ PlotResultsScript.m
@@ -21,20 +21,24 @@ simpathvector = ["simulations/ResultSim50050Nsim3000maxN240isotropic_boot_regula
 
 
 %% Visualize 500x50 simulations Rademacher regular
+% Loop over the simulation results
 for simpath = simpathvector
-    % Plot regular results
+    % load the simulation results
     load(simpath)
 
+    % get the parameters the simulation used
     dim     = simResults{1}.paramNoise.dim;
     method  = simResults{1}.quantEstim.params.method;
     weights = simResults{1}.quantEstim.params.weights;
 
-    % remove spaces in string
+    % remove spaces in string for the dimension
     tmp = num2str(dim);
     tmp = tmp(find(~isspace(tmp)));
+
     % define path identifier for plots
     picpathID = strcat( 'pics/', tmp, method, weights );
 
+    % get shortcuts for the method and weights for the title and labels
     if strcmp( method, 'regular' )
         methodflag = ' Reg. ';
     else
@@ -45,18 +49,22 @@ for simpath = simpathvector
     else
         weightsflag = 'Gauss';
     end
-
+    
+    % Create a title string for the plot
     titleSnippet = strcat(tmp,methodflag, weightsflag);
     clear weightsflag methodflag tmp;
 
-    %%% plot results for regular bootstrap
-    % cov rates
+    %%% plot results for the simulation
+    % plot cov rates linear bdry estimation 
     PlotResults( simResults, strcat(picpathID, 'linResult'), 'linbdry', ...
                  strcat(titleSnippet,'/linbdry'), 'y', 'variable')
+    % plot cov rates true bdry
     PlotResults( simResults, strcat(picpathID, 'trueResult')', 'truebdry', ...
                 strcat(titleSnippet,'/truedbdry'), 'y', 'variable')
-    %PlotResults( sim5050regResult, 'pics/5050regGausserodResult', 'erodbdry', 'CovRate: Reg. boots/erodbdry Estim', 'static')
-    % quantile estimates
+    % plot cov rates erod/dilation bdry estimation
+    PlotResults( simResults, strcat(picpathID, 'erodResult')', 'erodbdry', ...
+                strcat(titleSnippet,'/erodbdry'), 'y', 'variable')
+    % plot mean quantile estimates
     PlotQuantiles( simResults, strcat(picpathID, 'meanQuantiles'), titleSnippet)
 
     close all
